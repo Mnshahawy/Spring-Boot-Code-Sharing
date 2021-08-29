eb999b25117a952236cb30d1310acc6b38578bb0
Mohamed Elshahawi <elsha033@umn.edu>
2021-08-29 13:21:49 -0500

    Stage #4 solution

--- src/platform/util/Utils.java
+++ src/platform/util/Utils.java
@@ -5,14 +5,15 @@ import java.time.format.DateTimeFormatter;
 
 public class Utils {
     public static final String TEST_CODE = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
-    private static final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss a";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 
     public static String getFormattedDateTime(String isoDate){
         LocalDateTime dateTime = LocalDateTime.parse(isoDate);
-        return dateTime.format(dtFormatter);
+        return dateTime.format(DATE_TIME_FORMATTER);
     }
 
     public static String getFormattedDateTime(LocalDateTime dateTime){
-        return dateTime.format(dtFormatter);
+        return dateTime.format(DATE_TIME_FORMATTER);
     }
 }
